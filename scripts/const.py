66443b5f77ef9af5c0dc3f5d4bbcf0efa48bcecc
Artem Bisliouk <artem.bisliouk@gmail.com>
2025-05-29 00:10:35 -0400

    repository clean up

--- scripts/const.py
+++ scripts/const.py
@@ -4,9 +4,9 @@ LOGIT_BASED_CONFIDENCE = "logit_based_confidence"
 INTERNAL_BASED_CONFIDENCE = "internal_based_confidence"
 
 # Configuration
-MODEL_FREE = "gpt-3.5-turbo"
-MODEL_LOCAL = "Qwen-7B-Chat"
-INPUT_PATH_ORIGINAL = "data/math_translated_scored.json"
-INPUT_PATH_AUGMENTED = "data/reformatted_augmented_data.json"
+MODEL_GPT_3_5 = "gpt-3.5-turbo"
+MODEL_QWEN_7B = "Qwen-7B-Chat"
+INPUT_PATH_ORIGINAL = "data/original_dataset.json"
+INPUT_PATH_AUGMENTED = "data/reformatted_augmented_dataset.json"
 OUTPUT_DIR = "outputs"
 OUTPUT_DIR_COT = "outputs_cot"
